a51b90fc83a4b81ab678d3d02dd99f4b51bce1de
aikapavani29 <pavaniaika29@gmail.com>
2025-10-29 23:11:57 +0530

    Fix CSS import case sensitivity in Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./signup.css";
+import "./Signup.css";
 import { hashPassword } from "../utils/hash";
 
 const SignUp = () => {
